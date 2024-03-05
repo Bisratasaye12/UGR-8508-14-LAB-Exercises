47dc5838a56523dd100199efaf3cad0ee691e9df
Bisratasaye12 <138536048+Bisratasaye12@users.noreply.github.com>
2024-03-05 17:56:31 +0300

    Update LAB-1-Asynchronous-programming.dart

--- LAB-1-Asynchronous-programming.dart
+++ LAB-1-Asynchronous-programming.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 Future<String> fetchData() async {
-  // Simulate asynchronous operation (e.g., network request)
   await Future.delayed(const Duration(seconds: 2));
   return 'Data fetched successfully!';
 }
@@ -9,9 +8,16 @@ Future<String> fetchData() async {
 Future<void> main() async {
   print('Program starting...');
 
-  // Use await to pause execution until `fetchData` is complete
+  // Exercise 2
   String data = await fetchData();
-
   print(data);
+
+  // Exercise 3
+  fetchData().then((data) {
+    print('Data from Future.then: $data');
+  }).catchError((error) {
+    print('Error: $error');
+  });
+
   print('Program complete.');
 }
